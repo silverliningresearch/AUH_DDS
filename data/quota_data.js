2222ae177ea6f9154e12b355c591c48bf58c6bc5
lexuanbinh <lb@silverliningresearch.com>
2024-10-23 17:07:23 +0700

    update new files

--- data/quota_data.js
+++ data/quota_data.js
@@ -437,6 +437,70 @@
         "quota_id": "Arrival Immigration",
         "Terminal": "NA",
         "Quota": 200
+    },
+
+
+
+
+     {
+        "Month": "11-2024",
+        "quota_id": "Check-in & Departures Immigration",
+        "Terminal": "NA",
+        "Quota": 300
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Commercial - Retail and F&B",
+        "Terminal": "NA",
+        "Quota": 300
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Wayfinding, FIDS, Seatings & Gates, Wi-Fi - Arrivals",
+        "Terminal": "NA",
+        "Quota": 200
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Wayfinding, FIDS, Seatings & Gates, Wi-Fi - Departures",
+        "Terminal": "NA",
+        "Quota": 200
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Airport Facilities - Prayer Rooms - Departures",
+        "Terminal": "NA",
+        "Quota": 50
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Airport Facilities - Smoking Rooms - Arrivals",
+        "Terminal": "NA",
+        "Quota": 50
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Airport Facilities - Smoking Rooms - Departures",
+        "Terminal": "NA",
+        "Quota": 50
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Accessibility - Arrivals",
+        "Terminal": "NA",
+        "Quota": 30
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Accessibility - Departures",
+        "Terminal": "NA",
+        "Quota": 30
+    },
+    {
+        "Month": "11-2024",
+        "quota_id": "Arrival Immigration",
+        "Terminal": "NA",
+        "Quota": 200
     }
 
 ]    
