217495ccdaaa4bf620525d27b4f81fb9e3fcad1a
lexuanbinh <lb@silverliningresearch.com>
2024-10-31 16:57:34 +0700

    update new files

--- adhoc_dep/scripts/PrepareData.js
+++ adhoc_dep/scripts/PrepareData.js
@@ -127,7 +127,7 @@ function prepareInterviewData() {
          //&& (quota_data_temp[i].period_id == current_period)
         )
     {
-      quota_data_temp[i].Quota = quota_data_temp[i].Quota *2;//this is for 7000
+      quota_data_temp[i].Quota = quota_data_temp[i].Quota * 1;//this is for 3500
       quota_data.push(quota_data_temp[i]);
     }
   }
